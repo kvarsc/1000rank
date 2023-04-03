4f84922dfa13c7f01c3b2e3f9f2ef6e050246955
kvarsc <kylimar@gmail.com>
2023-04-03 17:34:01 -0400

    Installed sqlite_modern_cpp library and wrote initial DatabaseManager code.

--- 1000rank/DatabaseManager.cpp
+++ 1000rank/DatabaseManager.cpp
@@ -1,9 +1,9 @@
 #include "DatabaseManager.h"
 
-DatabaseManager::DatabaseManager()
-{
+DatabaseManager::DatabaseManager(const std::string& db_path) : db(db_path) {
+    // You can add any initialization code here if needed
 }
 
-DatabaseManager::~DatabaseManager()
-{
+DatabaseManager::~DatabaseManager() {
+    // Destructor will be called automatically, closing the connection to the database
 }
--- 1000rank/DatabaseManager.h
+++ 1000rank/DatabaseManager.h
@@ -1,8 +1,14 @@
 #pragma once
-class DatabaseManager
-{
-	public:
-		DatabaseManager();
-		~DatabaseManager();
-};
+#include <string>
+#include <vector>
+#include "hdr/sqlite3.h"
+#include "hdr/sqlite_modern_cpp.h"
 
+class DatabaseManager {
+public:
+    DatabaseManager(const std::string& db_path);
+    ~DatabaseManager();
+
+private:
+    sqlite::database db;
+};
